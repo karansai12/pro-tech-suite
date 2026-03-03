b3dee58824ced8791fafb9a2a83ef9a149b41c9b
karansai12 <karansaimd2003@gmail.com>
2026-03-03 14:02:51 +0530

    ZX-8 : Create an express backend and setup folder structure (#1)
    * Create an express backend and setup folder structure
    
    * error field removed
    
    * updated

---
+++ backend/src/controllers/auth.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express"
+
+export const register = (req: Request, res: Response) => {
+    res.status(201).json({ success: true, message: "Registration success", data: {} })
+}
