dcddb9c7bec5711ca41ae9369d9e4a8200709d81
karansai12 <karansaimd2003@gmail.com>
2026-03-11 12:08:16 +0530

    routes folder updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-import authRoutes from "./routes/auth"
+import authRoutes from "./routes/user"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
---
+++ backend/src/routes/task.ts
@@ -0,0 +1,10 @@
+import { verifyToken } from "../midleware/auth"
+import router from "./user"
+import { createTask, deleteTask, getAllTasks, getTaskById, getTaskByUserId, updateTask } from "../controllers/task"
+
+router.post("/createTask",verifyToken,createTask)
+router.get("/getAllTask",verifyToken,getAllTasks)
+router.delete("/deleteTask",verifyToken,deleteTask)
+router.get("/getTaskByUserId/:id",verifyToken,getTaskByUserId)
+router.put("/updateTask",verifyToken,updateTask)
+router.get("/getTaskById/:taskId",verifyToken,getTaskById)
